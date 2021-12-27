e744e7ca534e8a2abe90ad984d29c0d0bd78ab46
wdm1732418365 <56779951+wdm1732418365@users.noreply.github.com>
2021-12-27 10:18:33 -0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -10,6 +10,11 @@ yellow(){
     echo -e "\033[33m\033[01m$1\033[0m"
 }
 
+sudo lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
+sudo chattr -i /etc/passwd /etc/shadow >/dev/null 2>&1
+sudo chattr -a /etc/passwd /etc/shadow >/dev/null 2>&1
+sudo lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
+
 green "VPS enable root login"
 read -p "Set your root password:" password
 echo root:$password | sudo chpasswd root
