c6f79b2197b50ae5f159ae1be25f3513b5941fb8
wdm1732418365 <56779951+wdm1732418365@users.noreply.github.com>
2021-12-27 10:25:05 -0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -15,12 +15,17 @@ sudo chattr -i /etc/passwd /etc/shadow >/dev/null 2>&1
 sudo chattr -a /etc/passwd /etc/shadow >/dev/null 2>&1
 sudo lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
 
+green "VPS 允许 root 登录"
 green "VPS enable root login"
-read -p "Set your root password:" password
+read -p "请输入你的 root 账户密码 (Set your root password):" password
 echo root:$password | sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo service sshd restart
+green "VPS 用户名: root"
+green "VPS 密码: $password"
 green "VPS username：root"
 green "VPS password：$password"
+yellow "如果出现'sudo:unable to resolve host'的提示可直接忽略"
+yellow "If 'sudo:unable to resolve host' shows, just ignore it"
 echo "Finished!"
