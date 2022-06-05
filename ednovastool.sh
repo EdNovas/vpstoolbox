7572005766fb17d6fb4f2d2e1791fd09f51c513d
EdNovas <56779951+EdNovas@users.noreply.github.com>
2022-06-04 22:25:33 -0700

    Update ednovastool.sh

--- ednovastool.sh
+++ ednovastool.sh
@@ -84,32 +84,6 @@ function Get_Ip_Address(){
 
 function ddonekey(){
 	green "重装的系统说明："
-	echo "1、CentOS 7.7 (已关闭防火墙及SELinux，默认密码Pwd@CentOS)
-2、CentOS 7 (默认密码cxthhhhh.com)
-3、CentOS 8 (默认密码cxthhhhh.com)
-4、CentOS 6 (默认密码Minijer.com)
-5、Debian 11 (默认密码Minijer.com)
-6、Debian 10 (默认密码Minijer.com)
-7、Debian 9 (默认密码Minijer.com)
-8、Debian 8 (默认密码Minijer.com)
-9、Ubuntu 20.04 (默认密码Minijer.com)
-10、Ubuntu 18.04 (默认密码Minijer.com)
-11、Ubuntu 16.04 (默认密码Minijer.com)
-12、Windows Server 2019 (默认密码cxthhhhh.com)
-13、Windows Server 2016 (默认密码cxthhhhh.com)
-14、Windows Server 2012 (默认密码cxthhhhh.com)
-15、Windows Server 2012 Lite (默认密码nat.ee)
-16、Windows Server 2008 (默认密码cxthhhhh.com)
-17、Windows Server 2008 Lite (默认密码nat.ee)
-18、Windows Server 2003 (默认密码cxthhhhh.com)
-19、Windows Server 2003 Lite (默认密码WinSrv2003x86-Chinese)
-20、Windows 10 LTSC Lite (默认密码www.nat.ee)
-21、Windows 7 x86 Lite (默认密码Windows7x86-Chinese)
-22、Windows 7 Ent Lite (默认密码nat.ee)
-23、Windows 7 Ent Lite (UEFI支持甲骨文)(默认密码nat.ee)
-24、Windows Server 2008 Lite (UEFI支持甲骨文)(默认密码nat.ee)
-25、Windows Server 2012 Lite (UEFI支持甲骨文)(默认密码nat.ee)
-99、自定义镜像"
 	read -r -p "我已保存好默认root密码，并了解重装后VPS所有内容将会被清除[Y/n] " input
  
 	case $input in
@@ -320,7 +294,7 @@ function cloudflareddns(){
 	read -p "请输入你的Cloudflare邮箱地址: " cloudflareemail
 	read -p "请输入你的Cloudflare API: " cloudflareapi
 	read -p "请输入你的主域名（example.com）：" cloudflaredomain
-	read -p "请输入你的Hostname（homeserver.example.com中的homeserver即可）：" cloudflarehostname
+	read -p "请输入你的Hostname（homeserver.example.com）：" cloudflarehostname
 	sed -i "s/CFUSER=/CFUSER=$cloudflareemail/g" /root/cf-v4-ddns.sh
 	sed -i "s/CFKEY=/CFKEY=$cloudflareapi/g" /root/cf-v4-ddns.sh
 	sed -i "s/CFZONE_NAME=/CFZONE_NAME=$cloudflaredomain/g" /root/cf-v4-ddns.sh
