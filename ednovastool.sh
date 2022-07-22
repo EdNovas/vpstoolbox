9b93f8b28d3f4538d265300159a5c2ccd504d282
EdNovas <56779951+EdNovas@users.noreply.github.com>
2022-07-22 14:00:07 -0700

    Update ednovastool.sh

--- ednovastool.sh
+++ ednovastool.sh
@@ -38,34 +38,34 @@ else
 fi
 
 if ! type curl >/dev/null 2>&1; then 
-yellow "检测到curl未安装，安装中 "
-if [ $release = "Centos" ]; then
-    yum -y update && yum install curl -y
-else
-    apt-get update -y && apt-get install curl -y
-fi	   
+	yellow "检测到curl未安装，安装中 "
+		if [ $release = "Centos" ]; then
+		    yum -y update && yum install curl -y
+		else
+		    apt-get update -y && apt-get install curl -y
+		fi	   
 else
     green "curl已安装"
 fi
 
 if ! type wget >/dev/null 2>&1; then 
-yellow "检测到wget未安装，安装中 "
-if [ $release = "Centos" ]; then
-    yum -y update && yum install wget -y
-else
-    apt-get update -y && apt-get install wget -y
-fi	   
+	yellow "检测到wget未安装，安装中 "
+	if [ $release = "Centos" ]; then
+	    yum -y update && yum install wget -y
+	else
+	    apt-get update -y && apt-get install wget -y
+	fi	   
 else
     green "wget已安装"
 fi
 
 if ! type sudo >/dev/null 2>&1; then 
-yellow "检测到sudo未安装，安装中 "
-if [ $release = "Centos" ]; then
-    yum -y update && yum install sudo -y
-else
-    apt-get update -y && apt-get install sudo -y
-fi	   
+	yellow "检测到sudo未安装，安装中 "
+	if [ $release = "Centos" ]; then
+	    yum -y update && yum install sudo -y
+	else
+	    apt-get update -y && apt-get install sudo -y
+	fi	   
 else
     green "sudo已安装"
 fi
