fbe575876e5b6a308e1842f5c40c3ca04d1ad3b9
EdNovas <56779951+EdNovas@users.noreply.github.com>
2022-11-21 21:14:16 -0800

    Update ednovastool.sh

--- ednovastool.sh
+++ ednovastool.sh
@@ -253,7 +253,7 @@ function changedns(){
 }
 
 function addswap(){
-	wget https://www.moerats.com/usr/shell/swap.sh && bash swap.sh
+	wget https://raw.githubusercontent.com/wdm1732418365/vpstoolbox/main/root.sh && bash swap.sh
 }
 
 function nopingin(){
