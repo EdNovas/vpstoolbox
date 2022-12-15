22431afb3fd99a77ad5a9446a59b39424bd36ae2
EdNovas <56779951+EdNovas@users.noreply.github.com>
2022-12-14 18:12:01 -0800

    Update ednovastool.sh

--- ednovastool.sh
+++ ednovastool.sh
@@ -396,7 +396,7 @@ function routetracesimple(){
 
 # ==============part3=============
 function macka(){
-    wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/install.sh" && chmod 700 /root/install.sh && /root/install.sh
+    wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/ednovas/v2ray-agent/master/install.sh" && chmod 700 /root/install.sh && /root/install.sh
 }
 
 
